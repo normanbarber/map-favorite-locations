96838c946d702a6a03b73856c5e56e2344d581e3
Norman Barber <normanebarber@gmail.com>
2014-05-21 23:26:08 -0400

    updating mongo host config

--- config/default.js
+++ config/default.js
@@ -1,38 +1,38 @@
 module.exports = {
-    data: {
-        provider: [
-            'loginProvider.js',
-            'welcomeProvider.js',
-            'signupProvider.js'
-        ]
-    },
+	data: {
+		provider: [
+			'loginProvider.js',
+			'welcomeProvider.js',
+			'signupProvider.js'
+		]
+	},
 	db: {
-	  	servers: {
-            favorloca: {
-		    	host: 'localhost',
-		        port: 27017,
-                options: {
-                    server: {
-                        poolSize: 10
-                    },
-                    db: {
-                        journal: true
-                    }
-                }
-	        }
-	    },
-	    databases: {
-            favorloca: {
-	        	server: 'favoritelocations'
-	        }
-	    },
-	    collections: {
-	    	users: {
-	        	database: 'favoritelocations'
-		    },
-		    sessions: {
-	    	    database: 'favoritelocations'
-	        }
-	    }
+		servers: {
+			favorloca: {
+				host: 'localhost',
+				port: 27017,
+				options: {
+					server: {
+						poolSize: 10
+					},
+					db: {
+						journal: true
+					}
+				}
+			}
+		},
+		databases: {
+			favorloca: {
+				server: 'favoritelocations'
+			}
+		},
+		collections: {
+			users: {
+				database: 'favoritelocations'
+			},
+			sessions: {
+				database: 'favoritelocations'
+			}
+		}
 	}
 };
